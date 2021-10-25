571e123308c913caaa25ac274e38e004bc71a370
Linus Romland <hello@linusromland.com>
2021-10-25 10:49:01 +0200

    Shows popular repos on user

--- website/js/user.js
+++ website/js/user.js
@@ -1,7 +1,9 @@
+const nameTag = document.querySelector("#name")
 const usernameTag = document.querySelector("#username")
 const loadingArea = document.querySelector("#loadingArea")
 const main = document.querySelector("#main")
 const noOfCommits = document.querySelector("#noOfCommits")
+const popularRepos = document.querySelector("#popularRepos")
 
 let user;
 let name;
@@ -15,21 +17,33 @@ window.onload = async () => {
     let user = await getUser(params.user);
     if (!user) window.location = "index.html"
     console.log("Loaded user data!")
+    console.log(user)
 
     //Sets varible "name" to login or Username if exsits.
     name = user.name ? user.name : user.login;
     document.title = `${user.login} - Github Statistics`
-    username.textContent = name
+    nameTag.textContent = name
+    if (user.name) {
+        usernameTag.textContent = user.login
+        usernameTag.hidden = false;
+    }
 
     let commits = await getCommits(user.login)
     let chartData = await commitsToChart(commits)
     await renderChart(chartData.amount, chartData.day, "#chart", "line", "Commits")
+    let amount = chartData.popularRepositories.length > 5 ? 5 : chartData.popularRepositories.length
+    for (let i = 0; i < amount; i++) {
+        popularRepos.hidden = false;
+        const element = chartData.popularRepositories[i];
+        let li = document.createElement("li");
+        li.textContent = `Repo: ${element.repo} (Commits: ${element.amount})`
+        popularRepos.appendChild(li)
+    }
 
     loading(true)
     let noOfCommitsNum = 0;
     chartData.amount.forEach(amount => {
         noOfCommitsNum += parseInt(amount)
-        console.log(amount)
     });
     noOfCommits.textContent = `Commits (${chartData.day[0]} - ${chartData.day[chartData.day.length -1]}): ${noOfCommitsNum}`
 
@@ -45,8 +59,11 @@ commitsToChart = (commitArray) => {
     commitArray = commitArray.reverse(); //reverse array to get oldest first
     let object = {
         amount: [],
-        day: []
+        day: [],
+        popularRepositories: null,
     }
+
+    let popularRepositories = []
 
     for (let i = 0; i < commitArray.length; i++) { //Loops through commits
         const commit = commitArray[i];
@@ -67,8 +84,19 @@ commitsToChart = (commitArray) => {
         } else {
             object.amount[index] += commit.payload.commits.length
         }
+
+        let repoIndex = popularRepositories.findIndex((e) => e.repo === commit.repo.name) //Checks if repo is in repo array
+        if (repoIndex === -1) {
+            popularRepositories.push({
+                repo : commit.repo.name,
+                amount : commit.payload.commits.length
+            })
+        } else {
+            popularRepositories[repoIndex].amount += commit.payload.commits.length
+        }
     }
-    console.log(object)
+    popularRepositories.sort((a, b) => b.amount - a.amount)
+    object.popularRepositories = popularRepositories
     return object
 }
 
