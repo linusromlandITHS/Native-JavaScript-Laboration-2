98fea0be20885dfd793267d377b9993416b99f8f
Linus Romland <hello@linusromland.com>
2021-10-25 22:40:06 +0200

    Commented `repo.js`

--- website/js/repo.js
+++ website/js/repo.js
@@ -1,6 +1,10 @@
 window.onload = async () => {
+    //Convert URL to Params
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
+
+    //Gets repo from GitHub API
     let repo = await getRepo(params.repo);
+    //Redirects client if repo not found
     if (!repo) window.location = "index.html"
 }
