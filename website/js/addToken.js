4000960477ba6b92f74a842793fbb0ccb26f308b
Linus Romland <hello@linusromland.com>
2021-10-25 22:39:59 +0200

    Commented `addToken.js`

--- website/js/addToken.js
+++ website/js/addToken.js
@@ -1,10 +1,13 @@
+// Retrives of HTML DOM elements
 const form = document.querySelector("form")
 const GithubUsername = document.querySelector("#GithubUsername")
 const GithubPersonalToken = document.querySelector("#GithubPersonalToken")
 
+//Runs on submit on form
 form.addEventListener("submit", async event => {
     event.preventDefault();
     if (GithubPersonalToken && GithubUsername) {
+        //updates entries in localstorage and redirect client.
         await localStorage.setItem("githubusername", GithubUsername.value)
         await localStorage.setItem("githubpersonaltoken", GithubPersonalToken.value)
         window.location = "index.html"
