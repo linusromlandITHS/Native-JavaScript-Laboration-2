0b35d69b7c2436bcfe6da4f105d7a6fee6e1e6c9
Linus Romland <hello@linusromland.com>
2021-10-27 13:26:06 +0200

    Removed print to console

--- website/js/github.js
+++ website/js/github.js
@@ -7,7 +7,6 @@ if (_githubUsername && _githubPersonalToken) _params.headers = {
     "Authorization": `Basic ${btoa(_githubUsername + ":" + _githubPersonalToken)}`
 }
 
-console.log(_params);
 /**
  * @param  {string} username github accounts username 
  * 
