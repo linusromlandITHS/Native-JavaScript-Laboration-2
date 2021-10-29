71f9d0161ddc2860e376966ca5f1af427854faa5
Linus Romland <hello@linusromland.com>
2021-10-29 14:15:39 +0200

    init of api

---
+++ api/routes/apiRoutes.js
@@ -0,0 +1,11 @@
+module.exports = (function () {
+    //Dependencies import
+    const express = require("express");
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+      res.send('GET request to the api root')
+    })
+
+    return router;
+})();
