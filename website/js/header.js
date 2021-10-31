0eae896db1d0e5b6020117bb514680941853f539
Linus Romland <romlandlinus@gmail.com>
2021-10-31 19:29:26 +0100

    Added profile image to header

--- website/js/header.js
+++ website/js/header.js
@@ -1,6 +1,8 @@
 checkToken = async () => {
   const authArea = document.querySelector("#authArea");
+  const navItemsArea = document.querySelector("#navItemsArea")
   const personalToken = localStorage.getItem("githubpersonaltoken");
+
   if (personalToken) {
     let request = await fetch("https://api.github.com/user", {
         headers: {
@@ -12,7 +14,16 @@ checkToken = async () => {
         const name = data.name ? data.name : data.login;
         const p = document.createElement("p")
         p.textContent = `Authenticated as ${name}`
-        authArea.appendChild(p);
+
+        // Profile Image
+        const profileImage = document.createElement("img")
+        profileImage.alt = "Authenticated Profile Image"
+        profileImage.src = `${data.avatar_url}&s=55`
+        profileImage.width = 35
+        profileImage.height = 35
+        profileImage.style.borderRadius = "999px"
+
+        authArea.append(p, profileImage);
     }else{
         const a = document.createElement("a");
         a.classList = "nav-link";
