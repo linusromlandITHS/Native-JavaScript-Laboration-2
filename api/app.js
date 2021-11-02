18176ef0f052f39db9c2506da2f3b9088f63f735
Linus Romland <hello@linusromland.com>
2021-11-02 09:44:54 +0100

    .env for port if necceassary

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 const cors = require('cors')
 const ip = require('ip')
 
