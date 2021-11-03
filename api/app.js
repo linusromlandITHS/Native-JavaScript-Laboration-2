7fabe38a9d4a5dcb6a60f0ca0c8c02f26891de85
Linus Romland <hello@linusromland.com>
2021-11-03 22:44:14 +0100

    Changed formatter from `Beautify` to `Prettier`

--- api/app.js
+++ api/app.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const cors = require('cors')
-const ip = require('ip')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const ip = require("ip");
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.static("../website"));
 
@@ -14,5 +14,7 @@ app.use(express.static("../website"));
 app.use("/api", require("./routes/apiRoutes"));
 
 app.listen(port, () => {
-        console.log(`\nApp running at:\n- Local: \x1b[36mhttp://localhost:${ port }/\x1b[0m\n- Network \x1b[36mhttp://${ ip.address() }:${ port }/\x1b[0m\n\nTo run for production, run \x1b[36mnpm run start\x1b[0m`)
+    console.log(
+        `\nApp running at:\n- Local: \x1b[36mhttp://localhost:${port}/\x1b[0m\n- Network \x1b[36mhttp://${ip.address()}:${port}/\x1b[0m\n\nTo run for production, run \x1b[36mnpm run start\x1b[0m`
+    );
 });
